f2541e337e9a02ff527cf04d09635d46909f4681
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-01 19:31:48 +0300

    Added changes for code review session

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key_default')
 
-# DEBUG = os.getenv('DEVELOPMENT', False)
-DEBUG = True
+DEBUG = os.getenv('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = os.getenv('HOSTS_ALLOWED', '').split()
 
